7b642f0d664f50775f08a0adcf6d44f9d186c41c
Viktor Max <viktormax3@gmail.com>
2023-10-16 00:17:30 -0400

    prevenir que el ultimo tab se limpie al submit

--- js/scripts.js
+++ js/scripts.js
@@ -70,33 +70,27 @@ function showTab(n) {
 	// Muestra u oculta el botón prev según corresponda
 	document.getElementById("prevBtn").style.visibility = n === 0 ? "hidden" : "visible";
 	// Cambia el icono del botón next según corresponda
-	if (n === tabs.length - 1) {
-		nextBtn.setAttribute("class", "btn btn-1505c btn-lg fa-sharp fa-regular fa-paper-plane-top");
-	} else {
-		nextBtn.setAttribute("class", "btn btn-1505c btn-lg fa-solid fa-arrow-right");
-	}
+	nextBtn.className = "btn btn-1505c btn-lg" + (n === tabs.length - 1 ? " fa-sharp fa-regular fa-paper-plane-top" : " fa-solid fa-arrow-right");
 	// Actualiza los campos según la pestaña actual
 	validarYActualizarInputs();
 	deseleccionarTab();
 }
 // Define una función que recibe un parámetro n que indica si se avanza o retrocede una pestaña
 function nextPrev(n) {
-	// Si se avanza una pestaña y los campos de entrada no son válidos, termina la función
-	if (n == 1 && !validarYActualizarInputs()) return false;
 	// Llama a la función removeInputEvents para eliminar los eventos de entrada de los campos de entrada
 	removeInputEvents();
-	// Oculta el elemento con el índice currentTab
-	tabs[currentTab].style.display = "none";
-	// Actualiza el valor de currentTab sumando el valor de n
-	currentTab = currentTab + n;
 	// Si currentTab es mayor o igual al número de pestañas, envía el formulario y termina la función
-	if (currentTab >= tabs.length) {
+	if (currentTab + n >= tabs.length) {
 		document.getElementById("regForm").submit();
 		return false;
+	} else {
+		// Oculta el elemento con el índice currentTab
+		tabs[currentTab].style.display = "none"; 
+		// Actualiza el valor de currentTab sumando el valor de n
+		currentTab += n;
 	}
 	// Llama a la función showTab para mostrar la nueva pestaña
 	showTab(currentTab);
-	updateSpans();
 }
 // Define una función que deselecciona la pestaña actual
 function deseleccionarTab() {
@@ -118,31 +112,31 @@ function deseleccionarTab() {
 
 // Define una función que valida y actualiza los campos de entrada de la pestaña actual
 function validarYActualizarInputs() {
-	var i, valid = true;
+	var valid = true;
 	// Obtiene todos los elementos con la etiqueta "input" dentro de tabs y los asigna a una variable y
-	var y = tabs[currentTab].getElementsByTagName("input");
-	// Recorre todos los elementos de y y verifica su tipo y valor
-	for (i = 0; i < y.length; i++) {
-		switch (y[i].type) {
+	var inputs = tabs[currentTab].getElementsByTagName("input");
+	// Recorre todos los elementos de inputs y verifica su tipo y valor
+	for (var i = 0; i < inputs.length; i++) {
+		switch (inputs[i].type) {
 			case "email": // Si el tipo es "email"
-				// Verificar si el valor está vacío
-				if (y[i].value === '') {
-					y[i].classList.toggle("invalid", true);
+			// Verificar si el valor está vacío
+			// Verificar validez si no está vacío
+				if (inputs[i].value === '') {
+					inputs[i].classList.toggle("invalid", true);
 					valid = false;
-					// Verificar validez si no está vacío
-				} else if (!y[i].validity.valid) {
-					y[i].classList.toggle("invalid", true);
+				} else if (!inputs[i].validity.valid) {
+					inputs[i].classList.toggle("invalid", true);
 					valid = false; // Asigna false a la variable valid
 				} else { // Si se cumple elimina la clase "invalid" si está presente
-					y[i].classList.toggle("invalid", false);
+					inputs[i].classList.toggle("invalid", false);
 				}
 				break;
 			case "tel": // Si el tipo es "tel"
 				// Asigna el valor a una variable tlf
-				var tlf = y[i].value;
+				var tlf = inputs[i].value;
 				// Si el valor está vacío, agrega la clase "invalid" al elemento
 				if (tlf == "") {
-					y[i].classList.toggle("invalid", true);
+					inputs[i].classList.toggle("invalid", true);
 					valid = false; // Asigna false a la variable valid
 				} else { // Si el valor no está vacío
 					// Convierte el valor a un número y lo asigna a la variable tlf
@@ -151,44 +145,41 @@ function validarYActualizarInputs() {
 					var valido = !isNaN(tlf);
 					// Si el valor no es un número válido, agrega la clase "invalid" al elemento
 					if (!valido) {
-						y[i].classList.toggle("invalid", true);
+						inputs[i].classList.toggle("invalid", true);
 						valid = false; // Asigna false a la variable valid
 					} else { // Si el valor es un número válido, elimina la clase "invalid" si está presente
-						y[i].classList.toggle("invalid", false);
+						inputs[i].classList.toggle("invalid", false);
 					}
 				}
 				break;
 			case "radio": // Si el tipo es "radio"
 				// Obtiene el elemento con el mismo nombre que esté seleccionado y lo asigna a una variable radio
-				var radio = document.querySelector("input[name='" + y[i].name + "']:checked");
+				var radio = document.querySelector("input[name='" + inputs[i].name + "']:checked");
 				// Si no hay ningún elemento seleccionado, agrega la clase "invalid" al elemento
 				if (radio == null) {
-					y[i].classList.toggle("invalid", true);
+					inputs[i].classList.toggle("invalid", true);
 					valid = false; // Asigna false a la variable valid
 				} else { // Si hay un elemento seleccionado, comprueba si es válido y cambia la clase "invalid" según corresponda
-					y[i].classList.toggle("invalid", !radio.validity.valid);
+					inputs[i].classList.toggle("invalid", !radio.validity.valid);
 				}
 				break;
 			default: // Si el tipo es otro
 				// Si el valor está vacío, agrega la clase "invalid" al elemento
-				if (y[i].value == "") {
-					y[i].classList.toggle("invalid", true);
+				if (inputs[i].value == "") {
+					inputs[i].classList.toggle("invalid", true);
 					valid = false;
 				} else { // Si el valor no está vacío, elimina la clase "invalid" si está presente
-					y[i].classList.toggle("invalid", false);
+					inputs[i].classList.toggle("invalid", false);
 				}
 				break;
 		}
 		// Si el elemento no tiene el atributo "data-input-event", se lo agrega y le añade un evento "input"
-		if (!y[i].hasAttribute("data-input-event")) {
-			y[i].setAttribute("data-input-event", true);
-			// Usar una IIFE para crear una clausura que capture el valor de i
-			(function(j) {
-				y[j].addEventListener("input", function() {
-					// Llama a la función validarYActualizarInputs cada vez que se cambia el valor del elemento
-					validarYActualizarInputs();
-				});
-			})(i); // Pasar el valor de i como argumento a la IIFE
+		if (!inputs[i].hasAttribute("data-input-event")) {
+			inputs[i].setAttribute("data-input-event", true);
+			inputs[i].addEventListener("input", function() {
+				// Llama a la función validarYActualizarInputs cada vez que se cambia el valor del elemento
+				validarYActualizarInputs();
+			});
 		}
 	}
 	// Si el valor de valid es true, hace toggle de la clase "finish" en el indicador de la pestaña actual
@@ -226,12 +217,8 @@ function deseleccionar(e) {
 		// Obtiene el elemento actual
 		let elemento = elementos[i];
 		// Verifica si el elemento tiene el atributo data-haslistener
-		if (!elemento.getAttribute("data-haslistener")) {
-			// Si no lo tiene, se lo asigna con el valor "false"
-			elemento.setAttribute("data-haslistener", "false");
-		}
-		// Verifica si el valor del atributo data-haslistener es "false" y lo cambia a true
-		if (elemento.getAttribute("data-haslistener") == "false") {
+		const hasListener = elemento.getAttribute("data-haslistener") === "true";
+		if (!hasListener) {
 			elemento.setAttribute("data-haslistener", "true");
 			// Le añade el evento listener de tipo "click"
 			elemento.addEventListener("click", function() {
@@ -291,22 +278,16 @@ function clearRadioGroup(name) {
 }
 // Define una función que elimina los eventos de entrada de los campos de entrada de la pestaña actual
 function removeInputEvents() {
-	var i;
 	// Obtiene todos los elementos con la etiqueta "input" dentro de tabs y los asigna a una variable y
-	var y = tabs[currentTab].getElementsByTagName("input");
+	var inputs = tabs[currentTab].getElementsByTagName("input");
 	// Recorre todos los elementos de y y verifica si tienen el atributo "data-input-event"
-	for (i = 0; i < y.length; i++) {
-		if (y[i].hasAttribute("data-input-event")) {
+	for (var i = 0; i < inputs.length; i++) {
+		if (inputs[i].hasAttribute("data-input-event")) {
 			// Si lo tienen, se lo elimina y también elimina el evento "input" asociado
-			y[i].removeAttribute("data-input-event");
-			// Usa una expresión de función invocada inmediatamente (IIFE) para crear un closure
-			(function(index) {
-				// Pasa i como index al closure
-				y[index].removeEventListener("input", function() {
-					// El evento "input" llama a la función validarYActualizarInputs cada vez que se cambia el valor del elemento
-					validarYActualizarInputs();
-				});
-			})(i); // Invoca la IIFE con i como argumento
+			inputs[i].removeAttribute("data-input-event");
+			inputs[i].removeEventListener("input", function() {
+				validarYActualizarInputs();
+			});
 		}
 	}
 }
