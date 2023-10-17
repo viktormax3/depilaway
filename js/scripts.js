e0ed3cbbd6600c44372f0ea3a71db047a29af127
Viktor Max <viktormax3@gmail.com>
2023-10-17 17:19:48 -0400

    --single-quote

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,21 @@
 var currentTab = 0;
 // Define una variable que almacena el índice de la pestaña actual (inicialmente 0)
 let resetUltimo = false;
-// Define una variable que almacene los nombres de los grupos  de botones radio procesados 
+// Define una variable que almacene los nombres de los grupos  de botones radio procesados
 let gruposProcesados = [];
 // Obtener el div con id="stepContainer"
-var stepContainer = document.getElementById("stepContainer");
+var stepContainer = document.getElementById('stepContainer');
 // Obtener todos los divs con la clase tab
-var tabs = document.getElementsByClassName("tab");
+var tabs = document.getElementsByClassName('tab');
 // Obtener el boton nextBtn
-var nextBtn = document.getElementById("nextBtn");
+var nextBtn = document.getElementById('nextBtn');
 // Obtener todos los span con la clase step
-var steps = document.getElementsByClassName("step");
+var steps = document.getElementsByClassName('step');
 // Obtener el div que se muestra u oculta según el tratamiento
-var treatmentDiv = document.getElementById("applyTreatment");
+var treatmentDiv = document.getElementById('applyTreatment');
 // Obtener los radios que indican si se aplica tratamiento o no
-var treatedYes = document.getElementById("grupo2_0");
-var treatedNo = document.getElementById("grupo2_1");
+var treatedYes = document.getElementById('grupo2_0');
+var treatedNo = document.getElementById('grupo2_1');
 // Obtener el botón, su bisabuelo y su siguiente hermano para hacer un toggler de divs
 var btnToggler = document.getElementById('toggleBtn');
 var grandparent = btnToggler.parentElement.parentElement.parentElement;
@@ -27,8 +27,8 @@ var nextSibling = grandparent.nextElementSibling;
 function updateSpans() {
 	// Si el número de spans es menor que el número de divs, añadir los spans necesarios
 	while (steps.length < tabs.length) {
-		var newSpan = document.createElement("span");
-		newSpan.className = "step";
+		var newSpan = document.createElement('span');
+		newSpan.className = 'step';
 		// Validamos que haya un span actual
 		if (steps.length > 0) {
 			var currentStep = steps[currentTab];
@@ -44,7 +44,11 @@ function updateSpans() {
 		// Obtiene el siguiente span después del actual
 		var nextSpan = steps[currentTab].nextSibling;
 		// Si el siguiente span existe y es un span.step
-		if (nextSpan && nextSpan.tagName == "SPAN" && nextSpan.classList.contains("step")) {
+		if (
+			nextSpan &&
+			nextSpan.tagName == 'SPAN' &&
+			nextSpan.classList.contains('step')
+		) {
 			// Elimina ese span
 			nextSpan.parentNode.removeChild(nextSpan);
 		}
@@ -55,21 +59,26 @@ function updateSpans() {
 // Define una función que recibe un parámetro n que indica el índice de la pestaña a mostrar
 function showTab(n) {
 	// Agrega la clase active al step actual
-	steps[n].className += " active";
+	steps[n].className += ' active';
 	// Muestra el tab actual
-	tabs[n].style.display = "block";
+	tabs[n].style.display = 'block';
 	// Muestra u oculta el botón prev según corresponda
-	document.getElementById("prevBtn").style.visibility = n === 0 ? "hidden" : "visible";
+	document.getElementById('prevBtn').style.visibility =
+		n === 0 ? 'hidden' : 'visible';
 	// Cambia el icono del botón next según corresponda
-	nextBtn.className = "btn btn-1505c btn-lg " + (n === tabs.length - 1 ? "fa-sharp fa-regular fa-paper-plane-top" : "fa-solid fa-arrow-right");
+	nextBtn.className =
+		'btn btn-1505c btn-lg ' +
+		(n === tabs.length - 1
+			? 'fa-sharp fa-regular fa-paper-plane-top'
+			: 'fa-solid fa-arrow-right');
 	// Si el elemento no tiene el atributo "data-input-event", se lo agrega y le añade un evento "input"
-	const inputs = tabs[n].getElementsByTagName("input");
+	const inputs = tabs[n].getElementsByTagName('input');
 	for (let i = 0; i < inputs.length; i++) {
-		const inputEvent = inputs[i].getAttribute("data-input-event") === "true";
+		const inputEvent = inputs[i].getAttribute('data-input-event') === 'true';
 		if (!inputEvent) {
-			inputs[i].setAttribute("data-input-event", true);
+			inputs[i].setAttribute('data-input-event', true);
 			// Llama a la función validarYActualizarInputs cada vez que se cambia el valor del elemento
-			inputs[i].addEventListener("input", function() {
+			inputs[i].addEventListener('input', function () {
 				validarYActualizarInputs();
 			});
 		}
@@ -81,12 +90,15 @@ function showTab(n) {
 function nextPrev(n) {
 	// Si currentTab es mayor o igual al número de pestañas, envía el formulario y termina la función
 	if (currentTab + n >= tabs.length) {
-		document.getElementById("regForm").submit();
+		document.getElementById('regForm').submit();
 		return false;
 	} else {
 		// Oculta el elemento con el índice currentTab
-		tabs[currentTab].style.display = "none";
-		steps[currentTab].className = steps[currentTab].className.replace(" active", "");
+		tabs[currentTab].style.display = 'none';
+		steps[currentTab].className = steps[currentTab].className.replace(
+			' active',
+			'',
+		);
 		// Actualiza el valor de currentTab sumando el valor de n
 		currentTab += n;
 	}
@@ -113,29 +125,33 @@ function deseleccionarTab() {
 function validarYActualizarInputs() {
 	var valid = true;
 	// Obtiene todos los elementos con la etiqueta "input" dentro de tabs y los asigna a una variable y
-	var inputs = tabs[currentTab].getElementsByTagName("input");
+	var inputs = tabs[currentTab].getElementsByTagName('input');
 	// Recorre todos los elementos de inputs y verifica su tipo y valor
 	for (let i = 0; i < inputs.length; i++) {
 		switch (inputs[i].type) {
-			case "email": // Si el tipo es "email"
-				// Verifica validez con validity.valid o si está vacío  
-				inputs[i].classList.toggle("invalid",
-					!inputs[i].validity.valid || inputs[i].value === ""
+			case 'email': // Si el tipo es "email"
+				// Verifica validez con validity.valid o si está vacío
+				inputs[i].classList.toggle(
+					'invalid',
+					!inputs[i].validity.valid || inputs[i].value === '',
 				);
 				// Actualiza valid
-				valid = !inputs[i].classList.contains("invalid");
+				valid = !inputs[i].classList.contains('invalid');
 				break;
-			case "tel": // Si el tipo es "tel"
+			case 'tel': // Si el tipo es "tel"
 				// Comprueba vacío y NaN
-				inputs[i].classList.toggle("invalid",
-					inputs[i].value === "" || Number.isNaN(Number(inputs[i].value))
+				inputs[i].classList.toggle(
+					'invalid',
+					inputs[i].value === '' || Number.isNaN(Number(inputs[i].value)),
 				);
 				// Actualiza valor de valid
-				valid = valid && !inputs[i].classList.contains("invalid");
+				valid = valid && !inputs[i].classList.contains('invalid');
 				break;
-			case "radio": // Si el tipo es "radio"
+			case 'radio': // Si el tipo es "radio"
 				// Obtiene el elemento con el mismo nombre que esté seleccionado y lo asigna a una variable radio
-				var radio = document.querySelector("input[name='" + inputs[i].name + "']:checked");
+				var radio = document.querySelector(
+					"input[name='" + inputs[i].name + "']:checked",
+				);
 				// Si no hay ningún elemento seleccionado, agrega la clase "invalid" al elemento
 				if (!radio) {
 					valid = false; // Asigna false a la variable valid
@@ -143,16 +159,16 @@ function validarYActualizarInputs() {
 				break;
 			default: // Si el tipo es otro
 				// Si el valor está vacío, agrega la clase "invalid" al elemento
-				inputs[i].classList.toggle("invalid", inputs[i].value == "");
+				inputs[i].classList.toggle('invalid', inputs[i].value == '');
 				// Actualiza valor de valid
-				valid = valid && !inputs[i].classList.contains("invalid");
+				valid = valid && !inputs[i].classList.contains('invalid');
 				break;
 		}
 	}
 	// Basado en valor de valid se hacen un toggle a 2 clases
-	steps[currentTab].classList.toggle("finish", valid);
-	// Deshabilita nextBtn si valid es false  
-	nextBtn.classList.toggle("disabled", !valid);
+	steps[currentTab].classList.toggle('finish', valid);
+	// Deshabilita nextBtn si valid es false
+	nextBtn.classList.toggle('disabled', !valid);
 }
 // Define una función que recibe un parámetro grupo que indica el nombre de un grupo de elementos de tipo "radio"
 function deseleccionar(e) {
@@ -163,31 +179,31 @@ function deseleccionar(e) {
 	// Crea un objeto que almacene las funciones que se deben ejecutar para cada grupo
 	let funciones = {
 		grupo2: handleTreatedClick,
-		grupo3: function() {
+		grupo3: function () {
 			// Manejar el error del grupo3 al limpiar los botones radio cuando no existe
 			if (resetUltimo && !this.checked) {
 				ultimo = null;
 				this.checked = false;
 				resetUltimo = false;
 			}
-		}
+		},
 	};
 	for (let i = 0; i < elementos.length; i++) {
 		// Obtiene el elemento actual
 		let elemento = elementos[i];
 		// Verifica si el elemento tiene el atributo data-haslistener
-		const hasListener = elemento.getAttribute("data-haslistener") === "true";
+		const hasListener = elemento.getAttribute('data-haslistener') === 'true';
 		if (!hasListener) {
-			elemento.setAttribute("data-haslistener", "true");
+			elemento.setAttribute('data-haslistener', 'true');
 			// Le añade el evento listener de tipo "click"
-			elemento.addEventListener("click", function() {
+			elemento.addEventListener('click', function () {
 				// Si el nombre del elemento coincide con alguna de las propiedades del objeto funciones
 				if (funciones[elemento.name]) {
 					// Ejecuta la función correspondiente
 					funciones[elemento.name]();
 				}
 				// Si el elemento es el mismo que el último seleccionado y está marcado
-				if (this.getAttribute("id") == ultimo && elemento.checked) {
+				if (this.getAttribute('id') == ultimo && elemento.checked) {
 					// Lo deselecciona y limpia ultimo
 					elemento.checked = false;
 					ultimo = null;
@@ -195,7 +211,7 @@ function deseleccionar(e) {
 					validarYActualizarInputs();
 				} else {
 					// Si no, asigna el elemento a ultimo y lo selecciona
-					ultimo = this.getAttribute("id");
+					ultimo = this.getAttribute('id');
 					elemento.checked = true;
 				}
 			});
@@ -210,10 +226,10 @@ function deseleccionar(e) {
 function handleTreatedClick() {
 	// Obtener valor actual de treated para variar la clase tab segun sea el caso
 	if (treatedYes.checked || !treatedNo.checked) {
-		treatmentDiv.classList.add("tab");
+		treatmentDiv.classList.add('tab');
 		resetUltimo = treatedYes.checked ? true : false;
 	} else {
-		treatmentDiv.classList.remove("tab");
+		treatmentDiv.classList.remove('tab');
 		document.getElementById('applyTreatment').value = '0';
 		clearRadioGroup('grupo3');
 	}
@@ -239,6 +255,6 @@ btnToggler.addEventListener('click', () => {
 // La función toggleVisibility oculta el primer elemento y muestra el segundo
 function toggleVisibility(elem1, elem2) {
 	elem1.style.display = 'none';
-	elem1.classList.remove("d-flex");
+	elem1.classList.remove('d-flex');
 	elem2.style.display = 'block';
 }
